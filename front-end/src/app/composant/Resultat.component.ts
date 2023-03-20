fa819713d30f35d622f4cd9fc661393f4a953ac3
AUBINRAHMANI <aubin.rahmani@gmail.com>
2023-03-20 09:30:55 +0100

    Squelette de 'recuper les resulats des quizz

---
+++ front-end/src/app/composant/Resultat.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resultat',
+  templateUrl: './resultat.component.html',
+  styleUrls: ['./resultat.component.css']
+})
+
+export class ResultatComponent implements OnInit {
+  constructor() { }
+  ngOnInit() { }
+}

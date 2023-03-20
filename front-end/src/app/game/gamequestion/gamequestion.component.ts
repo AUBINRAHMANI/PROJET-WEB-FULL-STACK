55576b4a22c7ada186af6e86ebc9003b922ab9b9
Kaleb <dassou-ablam-kaleb.sika@etu-univ-cotedazur.fr>
2023-03-20 11:23:29 +0100

    Adaptation du model de gameinstance.model.ts

--- front-end/src/app/game/gamequestion/gamequestion.component.ts
+++ front-end/src/app/game/gamequestion/gamequestion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Answer, Question} from "../../../models/question.model";
 
 @Component({
   selector: 'app-gamequestion',
@@ -8,8 +9,31 @@ import { Component, OnInit } from '@angular/core';
 
 export class GamequestionComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit(): void {
+  }
+  currentQuestion!: Question ;
+
+  /*constructor(private gameService: GameService) {
+    this.gameService.currentQuestion$.subscribe((question) => {
+      this.currentQuestion = question;
+    });
+    this.gameService.answerStatus$.subscribe((isCorrect) => {
+      if (isCorrect !== null) {
+        this.handleAnswerStatus(isCorrect);
+      }
+    });
+  }*/
+
+
+  onAnswerSelected(selectedAnswer: Answer) {
+   /* this.gameService.registerAnswer(selectedAnswer);*/
+    console.log('Réponse sélectionnée:', selectedAnswer);
+  }
+  handleAnswerStatus(isCorrect: boolean): void {
+    if (isCorrect) {
+      // Gérer la réponse correcte (afficher un message, passer à la question suivante, etc.)
+    } else {
+      // Gérer la réponse incorrecte (afficher un message, proposer de retenter, etc.)
+    }
   }
 }

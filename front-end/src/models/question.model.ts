87f47fb46e9be7403929ea3976ff88e77d4e78f1
Kaleb <dassou-ablam-kaleb.sika@etu-univ-cotedazur.fr>
2023-03-25 18:09:38 +0100

    Modifications de question.model.ts ajout de question SELECTEDANSWER ET UN AUTRE

--- front-end/src/models/question.model.ts
+++ front-end/src/models/question.model.ts
@@ -1,10 +1,12 @@
 export interface Answer {
+  answerId : number;
   type?: string;
   value: string;
   isCorrect: boolean;
 }
 
 export interface Question {
+  selectedAnswerIndex?: number;
   id: string;
   label: string;
   answers: Answer[];

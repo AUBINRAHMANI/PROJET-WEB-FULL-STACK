c8576e15fdbbbe220d0fe1449e98c5bf838add06
Kaleb <dassou-ablam-kaleb.sika@etu-univ-cotedazur.fr>
2023-03-25 15:54:29 +0100

    Modification du Game-service pour les attentes a venir

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,30 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {GamepageComponent} from "./game/gamepage/gamepage.component";
 import {GamequestionComponent} from "./game/gamequestion/gamequestion.component";
-import {GameanswerComponent} from "./game/gameanswer/gameanswer.component";
-import {GameinstanceComponent} from "./game/gameinstance/gameinstance.component";
 import {ResultatComponent} from "./composant/Resultat.component";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatCardModule} from "@angular/material/card";
 import {MatGridListModule} from "@angular/material/grid-list";
+import {HttpClientModule} from "@angular/common/http";
+import {GameInstanceComponent} from "./game/gameinstance/game-instance.component";
+import {GamePageComponent} from "./game/gamepage/gamepage.component";
+import {GameAnswerComponent} from "./game/gameanswer/gameanswer.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     ResultatComponent,
-    GamepageComponent,
+    GamePageComponent,
     GamequestionComponent,
-    GameanswerComponent,
-    GameinstanceComponent
+    GameAnswerComponent,
+    GameInstanceComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatGridListModule,
     MatCardModule,
-    MatGridListModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front-end/src/service/game.service.ts
+++ front-end/src/service/game.service.ts
@@ -1,24 +1,92 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {BehaviorSubject } from "rxjs";
-import {Question} from "../models/question.model";
-import {QUESTION_CAPITALES} from "../mocks/quiz-list.mock";
+import { HttpClient } from '@angular/common/http';
+import {BehaviorSubject, map, Observable} from 'rxjs';
+import { Quiz } from '../models/quiz.model';
+import { Question } from "../models/question.model";
+import { QUIZ_LIST } from "../mocks/quiz-list.mock";
+
 @Injectable({
   providedIn: 'root'
 })
-
 export class GameService {
+  private quizList: Quiz[] = [];
+  private _currentQuizIndex = 0;
+  private _currentQuestionIndex = 0;
 
-  private questions: Question[] = [];
-
-  public questions$: BehaviorSubject<Question[]> = new BehaviorSubject(this.questions);
+  public quizList$: BehaviorSubject<Quiz[]> = new BehaviorSubject(this.quizList);
+  public currentQuiz$: BehaviorSubject<Quiz> = new BehaviorSubject(this.quizList[this.currentQuizIndex]);
+  public currentQuestion$: BehaviorSubject<Question> = new BehaviorSubject(this.quizList[this.currentQuizIndex].questions[this.currentQuestionIndex]);
 
   constructor(private http: HttpClient) {
-    this.retrieveQuestions();
+    this.retrieveQuizList();
+  }
+  get currentQuizIndex(): number {
+    return this._currentQuizIndex;
   }
 
-  retrieveQuestions(): void {
-    this.questions=QUESTION_CAPITALES;
-    this.questions$.next(this.questions);
+  set currentQuizIndex(index: number) {
+    this._currentQuizIndex = index;
+    this.currentQuiz$.next(this.quizList[this._currentQuizIndex]);
+    this.currentQuestionIndex = 0;
   }
+
+  get currentQuestionIndex(): number {
+    return this._currentQuestionIndex;
+  }
+
+  set currentQuestionIndex(index: number) {
+    this._currentQuestionIndex = index;
+    this.currentQuestion$.next(this.quizList[this.currentQuizIndex].questions[this._currentQuestionIndex]);
+  }
+
+
+  public retrieveQuizList(): void {
+    this.quizList = QUIZ_LIST;
+    this.quizList$.next(this.quizList);
+    this.currentQuizIndex = 0;
+    this.currentQuiz$.next(this.quizList[this.currentQuizIndex]);
+    this.currentQuestionIndex = 0;
+    this.currentQuestion$.next(this.quizList[this.currentQuizIndex].questions[this.currentQuestionIndex]);
+  }
+
+  retrieveQuestions(quizId: string): void {
+    const quiz = this.quizList.find(q => q.id === quizId);
+    if (quiz) {
+      this.currentQuizIndex = this.quizList.indexOf(quiz);
+      this.currentQuiz$.next(quiz);
+      this.currentQuestionIndex = 0;
+      this.currentQuestion$.next(quiz.questions[this.currentQuestionIndex]);
+    } else {
+      console.log(`Le quiz avec l'identifiant ${quizId} n'a pas été trouvé.`);
+    }
+  }
+
+  nextQuestion(): void {
+    if (this.currentQuestionIndex < this.quizList[this.currentQuizIndex].questions.length - 1) {
+      this.currentQuestionIndex++;
+      this.currentQuestion$.next(this.quizList[this.currentQuizIndex].questions[this.currentQuestionIndex]);
+    } else {
+      // si c'est la dernière question du quiz, on passe au quiz suivant
+      this.nextQuiz();
+    }
+  }
+
+  nextQuiz(): void {
+    if (this.currentQuizIndex < this.quizList.length - 1) {
+      this.currentQuizIndex++;
+      this.currentQuiz$.next(this.quizList[this.currentQuizIndex]);
+      this.currentQuestionIndex = 0;
+      this.currentQuestion$.next(this.quizList[this.currentQuizIndex].questions[this.currentQuestionIndex]);
+    } else {
+      // si c'est le dernier quiz, on affiche un message de fin de jeu ou on redirige vers la page d'accueil, etc.
+      console.log('Fin du jeu');
+    }
+  }
+
+  getQuiz(quizId: string): Observable<Quiz | undefined> {
+    return this.quizList$.pipe(
+      map((quizList: Quiz[]) => quizList.find((quiz: Quiz) => quiz.id === quizId))
+    );
+  }
+
 }

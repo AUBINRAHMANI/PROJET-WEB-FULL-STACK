e9fe5b114e8eb28fb202d439c588c2a448ca34e2
Kaleb <dassou-ablam-kaleb.sika@etu-univ-cotedazur.fr>
2023-03-25 15:59:23 +0100

    Modification du fichier TS de gamequestion.component.ts

--- front-end/src/app/game/gamequestion/gamequestion.component.ts
+++ front-end/src/app/game/gamequestion/gamequestion.component.ts
@@ -1,41 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Answer, Question } from '../../../models/question.model';
+import { GameService } from '../../../service/game.service';
+import {Quiz} from "../../../models/quiz.model";
 
 @Component({
   selector: 'app-gamequestion',
   templateUrl: './gamequestion.component.html',
   styleUrls: ['./gamequestion.component.scss']
 })
-
 export class GamequestionComponent implements OnInit {
-
   questionlist: Question[] = [];
+  currentQuestion!: Question;
 
-  constructor(public gameservice : GameService) {
-    this.gameservice.questions$.subscribe((questions : Question[]) => {
-      this.questionlist = questions;
-    });
-  }
+  @Output() answerSelected: EventEmitter<Answer> = new EventEmitter();
 
-  ngOnInit(): void {
-  }
-  currentQuestion!: Question ;
-
-  /*constructor(private gameService: GameService) {
-    this.gameService.currentQuestion$.subscribe((question) => {
+  constructor(public gameservice: GameService) {
+    this.gameservice.currentQuestion$.subscribe((question: Question) => {
       this.currentQuestion = question;
     });
-    this.gameService.answerStatus$.subscribe((isCorrect) => {
-      if (isCorrect !== null) {
-        this.handleAnswerStatus(isCorrect);
-      }
+    this.gameservice.quizList$.subscribe((quizList: Quiz[]) => {
+      this.questionlist = quizList[this.gameservice.currentQuizIndex].questions;
+      this.currentQuestion = this.questionlist[this.gameservice.currentQuestionIndex];
     });
-  }*/
-
-
-  onAnswerSelected(selectedAnswer: Answer) {
-   /* this.gameService.registerAnswer(selectedAnswer);*/
-    console.log('Réponse sélectionnée:', selectedAnswer);
   }
+
+  ngOnInit(): void {}
+
+  onAnswerSelected(question: Question, answer: Answer) {
+    console.log(`La réponse "${answer.value}" a été sélectionnée pour la question "${question.label}".`);
+    this.answerSelected.emit(answer);
+  }
+
+  nextQuestion(): void {
+    this.gameservice.nextQuestion();
+  }
+
   handleAnswerStatus(isCorrect: boolean): void {
     if (isCorrect) {
       // Gérer la réponse correcte (afficher un message, passer à la question suivante, etc.)

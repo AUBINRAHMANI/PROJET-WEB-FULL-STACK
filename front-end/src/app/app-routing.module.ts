f140d40bffbc87688b6ec6dbbdf1545afeecb978
Kaleb <dassou-ablam-kaleb.sika@etu-univ-cotedazur.fr>
2023-03-25 18:31:45 +0100

    Ajout d'un route

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {GamePageComponent} from "./game/game-page/gamepage.component";
+import {GameInstanceComponent} from "./game/game-instance/game-instance.component";
 
 const routes: Routes = [
+  { path: '', component: GameInstanceComponent },
   { path: 'gamepage', component: GamePageComponent },
 ];
 

2bdddee0bd5430dacb5f48421f00634c82c2257d
Kaleb <dassou-ablam-kaleb.sika@etu-univ-cotedazur.fr>
2023-03-25 16:03:18 +0100

    Modification de gameInstanceComponant

--- front-end/src/app/game/gameinstance/game-instance.component.ts
+++ front-end/src/app/game/gameinstance/game-instance.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import {Quiz} from "../../../models/quiz.model";
+import {GameService} from "../../../service/game.service";
+
 
 @Component({
-  selector: 'app-gameinstance',
+  selector: 'app-game-instance',
   templateUrl: './game-instance.component.html',
   styleUrls: ['./game-instance.component.scss']
 })
-
 export class GameInstanceComponent implements OnInit {
 
-    constructor() { }
+  quizList$: Observable<Quiz[]> = new Observable<Quiz[]>();
 
-    ngOnInit(): void {
-    }
+  constructor(private gameService: GameService) { }
+
+  ngOnInit(): void {
+    this.quizList$ = this.gameService.getQuizList();
+    this.quizList$.subscribe((quizList: Quiz[]) => {
+      // Traiter la valeur de quizList ici
+    });
+  }
+
 }

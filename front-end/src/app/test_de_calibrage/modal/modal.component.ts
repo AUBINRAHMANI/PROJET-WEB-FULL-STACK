fb284df1093c5092a00e176d5586401001a290a5
Kaleb <dassou-ablam-kaleb.sika@etu-univ-cotedazur.fr>
2023-04-21 17:11:50 +0200

    modification du modal

--- front-end/src/app/test_de_calibrage/modal/modal.component.ts
+++ front-end/src/app/test_de_calibrage/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -6,11 +6,19 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./modal.component.scss']
 })
 
-export class ModalComponent {
+export class ModalComponent implements OnChanges {
   @Input() title: string = 'Modal';
   @Input() message: string = '';
   @Input() show: boolean = false;
   @Output() close: EventEmitter<void> = new EventEmitter<void>();
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['show'] && changes['show'].currentValue) {
+      setTimeout(() => {
+        this.close.emit();
+      }, 3000);
+    }
+  }
 
   onClose(): void {
     this.close.emit();

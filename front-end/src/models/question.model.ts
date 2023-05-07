08b0ca7d19c977ddd444f01d02eac73326eeeb6e
Kaleb <dassou-ablam-kaleb.sika@etu-univ-cotedazur.fr>
2023-05-07 22:59:33 +0200

    refactor des mocks

--- front-end/src/models/question.model.ts
+++ front-end/src/models/question.model.ts
@@ -1,15 +1,51 @@
-export interface Answer {
-  answerId : number;
-  type?: string;
-  value: string;
-  isCorrect: boolean;
+export class Answer {
+  constructor(
+    public answerId: number,
+    public value: string,
+    public isCorrect: boolean,
+    public type?: string
+  ) {}
 }
 
-export interface Question {
+
+export class Question {
+  constructor(
+    public id: string,
+    public label: string,
+    public answers: Answer[]
+  ) {}
   selectedAnswerIndex?: number;
+  // @ts-ignore
   id: string;
+  // @ts-ignore
   label: string;
+  // @ts-ignore
   answers: Answer[];
+  // Propriétés et constructeur de la classe Question
+
+  getCorrectAnswer(): Answer | undefined {
+    return this.answers.find((answer) => answer.isCorrect);
+  }
+
+  getRandomIncorrectAnswer(): Answer | undefined {
+    const incorrectAnswers = this.answers.filter((answer) => !answer.isCorrect);
+    if (incorrectAnswers.length === 0) {
+      return undefined;
+    }
+    const randomIndex = Math.floor(Math.random() * incorrectAnswers.length);
+    return incorrectAnswers[randomIndex];
+  }
+
+  getMiniusQuestions(): Answer[] {
+    const correctAnswer = this.getCorrectAnswer();
+    const incorrectAnswer = this.getRandomIncorrectAnswer();
+    if (!correctAnswer || !incorrectAnswer) {
+      return [];
+    }
+    return [correctAnswer, incorrectAnswer];
+  }
+
+
 }
 
 

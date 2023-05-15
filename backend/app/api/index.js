f17dbc3fb3cd5de2eba995a8a6faaaa05b3c6f55
ChatelainClement <clementchatelain@hotmail.fr>
2023-05-15 10:29:18 +0200

    Suppression de l'ancien backend vide

--- backend/app/api/index.js
+++
@@ -1,6 +0,0 @@
-const { Router } = require('express')
-
-const router = new Router()
-router.get('/status', (req, res) => res.status(200).json('ok'))
-
-module.exports = router
--- backend/app/utils/logger.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-console */
-const originalConsoleLog = console.log
-console.log = function consoleLog(...args) {
-  const newArguments = [`[${new Date().toISOString()}]`, ...args]
-  return originalConsoleLog.apply(this, newArguments)
-}
-console.info = console.log
-
-module.exports = console

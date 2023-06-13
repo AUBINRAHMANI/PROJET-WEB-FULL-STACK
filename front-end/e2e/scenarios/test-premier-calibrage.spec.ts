419fd2907169a7d6781bff2d600a723b3d2a9e09
ChatelainClement <clementchatelain@hotmail.fr>
2023-06-13 19:22:37 +0200

    Test pour effectuer le calibrage

--- front-end/e2e/scenarios/test-premier-calibrage.spec.ts
+++ front-end/e2e/scenarios/test-premier-calibrage.spec.ts
@@ -50,3 +50,32 @@ test.describe('Présence de calibrage', () => {
     expect(appCalibrageElement).toBeNull();
   });
 });
+
+test.describe('Prise en compte du calibrage', () => {
+  test('Calibrage jamais effectué', async ({ page }) => {
+    await page.goto(testUrl);
+
+    //creation des fixtures
+    const connexionFixture = new ConnexionFixture(page);
+
+    await page.waitForSelector('.profil-liste .profil-item span');
+
+    // Cliquer sur le div qui contient "Durcit Pierre"
+    await page.click('.profil-liste .profil-item >> text="Durcit Pierre"');
+
+    // Attendre que l'élément profil-selectionne soit visible
+    await page.waitForSelector('.profil-selectionne');
+
+    // Cliquer sur le div profil-selectionne
+    await page.click('.profil-selectionne .profil-item');;
+
+    // Cliquer sur le bouton "Calibrer"
+    await page.click('.calibrage-bouton');
+
+    // Attendre que l'élément app-calibrage soit visible
+    await page.waitForSelector('app-calibrage');
+
+
+
+  });
+});

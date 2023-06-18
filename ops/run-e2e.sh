cb33c1f15103608c0c9b87907cf40919f9df1920
Kaleb <dassou-ablam-kaleb.sika@etu-univ-cotedazur.fr>
2023-06-18 10:59:35 +0200

    modification des .sh

--- ops/run-e2e.sh
+++ ops/run-e2e.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Se déplacer vers le répertoire du script
-cd "$(dirname "$0")"
+#cd "$(dirname "$0")"
 
 # Construire et exécuter les conteneurs de tests avec docker-compose
 docker-compose -f docker-compose-e2e.yml build
--- ops/run.sh
+++ ops/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Se déplacer vers le répertoire du script
-cd "$(dirname "$0")"
+#cd "$(dirname "$0")"
 
 # Construire et exécuter les conteneurs avec docker-compose
 docker-compose -f docker-compose.yml build
